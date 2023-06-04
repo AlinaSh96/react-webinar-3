b39f77d9e4db24f2a60f7cecb2ad23ff16e4de9f
AlinaSh96 <alina30081996@gmail.com>
2023-06-05 00:10:41 +0300

    add spinner

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 import {Routes, Route} from 'react-router-dom';
 import useSelector from "../hooks/use-selector";
+import useTranslate from "../hooks/use-translate";
 import Main from "./main";
 import Basket from "./basket";
 import Article from "./article";
@@ -7,34 +8,36 @@ import Authorization from './authorization';
 import Profile from './profile';
 import useStore from "../hooks/use-store";
 import useInit from "../hooks/use-init";
-import RequireAuth from '../components/require-auth';
+import RequireAuth from '../containers/require-auth';
+import Spinner from '../components/spinner';
+
 /**
  * Приложение
  * @returns {React.ReactElement}
  */
 function App() {
   const store = useStore();
-  const activeModal = useSelector(state => state.modals.name);
-  const select = useSelector((state) => ({
-    isAuth: state.profile.isAuth,
-    isLoading: state.authorization.waiting,
-  }));
-
+  const {t} = useTranslate();
 
   useInit(() => {
     store.actions.profile.loginByToken()
   }, [], true);
 
+  const activeModal = useSelector(state => state.modals.name);
+  const select = useSelector((state) => ({
+    isAuth: state.profile.isAuth,
+    isLoading: state.profile.waiting,
+  }));
 
+ if (select.isLoading) return <Spinner active={true}>{t('loading')}...</Spinner>
   return (
     <>
       <Routes>
         <Route path={''} element={<Main/>}/>
         <Route path={'/articles/:id'} element={<Article/>}/>
-        <Route path={'/login'} element={<RequireAuth to='/profile' auth={!(select.isAuth)}>{<Authorization/>}</RequireAuth>}/>
-        <Route path={'/profile'} element={<RequireAuth to='/login' auth={(select.isAuth)}>{<Profile/>}</RequireAuth>}/>
+        <Route path={'/login'} element={<RequireAuth  to='/profile' auth={!(select.isAuth)}>{<Authorization/>}</RequireAuth>}/>
+        <Route path={'/profile'} element={<RequireAuth  to='/login' auth={(select.isAuth)}>{<Profile/>}</RequireAuth>}/>
       </Routes>
-
       {activeModal === 'basket' && <Basket/>}
 
     </>
--- src/containers/require-auth/index.js
+++ src/containers/require-auth/index.js
@@ -2,10 +2,10 @@ import { memo } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import PropTypes from "prop-types";
 
-function RequireAuth({ auth,  children, to = "/"}) {
+function RequireAuth({ auth,  children, to = "/", isLoading}) {
     const location = useLocation(); 
     if (!auth) {
-        return <Navigate to={to} state={{ from: location }} replace/>;
+        return  <Navigate to={to} state={{ from: location }} replace/>;
     }
     return children;
 }
